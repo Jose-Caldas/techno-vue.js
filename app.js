47a9551a54b1f19f1c83dc709cacde596c824b28
José Caldas <jrcaldas@yahoo.com.br>
2023-09-10 16:19:46 -0300

    add local storage

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ const vm = new Vue({
       if (this.carrinho.length) {
         this.carrinho.forEach((item) => {
           total += item.preco
-          console.log(item)
         })
       }
       return total
@@ -58,8 +57,19 @@ const vm = new Vue({
     removerItem(index) {
       this.carrinho.splice(index, 1)
     },
+    checarLocalStorage() {
+      if (window.localStorage.carrinho) {
+        this.carrinho = JSON.parse(window.localStorage.carrinho)
+      }
+    },
+  },
+  watch: {
+    carrinho() {
+      window.localStorage.carrinho = JSON.stringify(this.carrinho)
+    },
   },
   created() {
     this.fetchProdutos()
+    this.checarLocalStorage()
   },
 })
